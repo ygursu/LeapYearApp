4b0088b9cfdf29ef0f93f5f9f5ec2e6d9df33620
ygursu <yasemingrs@gmail.com>
2024-02-09 23:13:57 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp52/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+bool ArtikYilMi(int yil)
+{
+    return
+        yil % 4 == 0 &&
+        (yil % 100 != 0 || yil % 400 == 0);
+}
+
+static bool ArtikYilMi(int yil)
+{
+    return
+        yil % 4 == 0 &&
+        (yil % 100 != 0 || yil % 400 == 0);
+}
+
+static void Main(string[] args)
+{
+    for (int i = 1900, sira = 1; i <= 2100; i++)
+    {
+        if (ArtikYilMi(i))
+        {
+            Console.Write(i + " ");
+            if (sira++ % 7 == 0)
+                Console.WriteLine();
+        }
+    }
+
+    Console.ReadKey();
+}
+Çıktı:
+
+
+
+static bool ArtikYilMi(int yil)
+{
+    return
+        yil % 4 == 0 &&
+        (yil % 100 != 0 || yil % 400 == 0);
+}
+
+static void Main(string[] args)
+{
+    for (int i = 1900, sira = 1; i <= 2100; i++)
+    {
+        if (DateTime.IsLeapYear(i))
+        {
+            Console.Write(i + " ");
+            if (sira++ % 7 == 0)
+                Console.WriteLine();
+        }
+    }
+
+    Console.ReadKey();
+}
